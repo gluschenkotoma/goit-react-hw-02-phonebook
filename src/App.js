aaa4b7392bd4fda9de7266419e88ce2b844fa43e
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-11-05 13:25:39 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
-import { v1 as uuid } from 'uuid';
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
+import { v1 as uuid } from "uuid";
 
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', phoneNumber: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', phoneNumber: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', phoneNumber: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', phoneNumber: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", phoneNumber: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", phoneNumber: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", phoneNumber: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", phoneNumber: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
 
   addContact = ({ name, phoneNumber }) => {
@@ -27,29 +26,31 @@ class App extends Component {
     this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
   };
 
-  isInContacts = name => {
+  isInContacts = (name) => {
     name = name.toLowerCase();
     return (
-      this.state.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(name),
+      this.state.contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(name)
       ).length > 0
     );
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
     }));
   };
 
-  changeFilter = e => {
+  changeFilter = (e) => {
     this.setState({ filter: e.target.value });
   };
 
   getFilteredContacts = () => {
     const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
+    return this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
